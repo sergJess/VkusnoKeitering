d8d4e075962c2a84fb5e7153c411d136c20b85e5
sergJess <serg.fantasy@yandex.ru>
2025-07-30 12:47:00 +0300

    feat: add page service keitering with styles and script

---
+++ src/js/service-keitering/service-keitering.js
@@ -0,0 +1,11 @@
+// menu
+const burger = document.querySelector('.navigation-burger');
+burger.onclick = ()=>{
+    const mobileMenu = document.querySelector('.header__block-nav');
+    mobileMenu.classList.add('header__block-nav_open');
+}
+const cross = document.querySelector('.navigation-cross__click');
+cross.onclick = () => {
+    const mobileMenu = document.querySelector('.header__block-nav');
+    mobileMenu.classList.remove('header__block-nav_open');
+}
