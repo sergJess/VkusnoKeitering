f32a3d98612544f0ed07137d0d67c55de8d23101
sergJess <serg.fantasy@yandex.ru>
2025-08-12 19:08:32 +0300

    feat: add new styles and script

--- src/js/vacancy/vacancy.js
+++ src/js/vacancy/vacancy.js
@@ -1,26 +1,45 @@
-// menu
-const burger = document.querySelector('.navigation-burger');
+const header = document.getElementById('header-id');
+//menu
+const burger = header.querySelector('.navigation-burger');
 burger.onclick = ()=>{
-    const mobileMenu = document.querySelector('.header__block-nav');
+    const mobileMenu = header.querySelector('.header__block-nav');
     mobileMenu.classList.add('header__block-nav_open');
 }
-const cross = document.querySelector('.navigation-cross__click');
+const cross = header.querySelector('.navigation-cross__click');
 cross.onclick = () => {
-    const mobileMenu = document.querySelector('.header__block-nav');
+    const mobileMenu = header.querySelector('.header__block-nav');
     mobileMenu.classList.remove('header__block-nav_open');
 }
+//order-call-form
+const orderCallButtons = header.querySelectorAll('.order-call-button_btn');
+const crossOrderCallForm = document.getElementById('order-call-form-cross-id');
+const orderCallForm = document.getElementById('order-call-form-block-id');
+crossOrderCallForm.onclick = () => {
+     orderCallForm.classList.remove('order-call-form-block_show');
+}
+for(let i = 0, length = orderCallButtons.length; i < length; i++){
+    orderCallButtons[i].onclick = () => {
+        orderCallForm.classList.add('order-call-form-block_show');
+    }
+}
+// navigation show submenu
+const navigationLists = header.querySelectorAll('.navigation__list');
+for(let i = 0, length = navigationLists.length; i < length; i++){
+navigationLists[i].onclick = () => {
+    const navigationSubmenu = navigationLists[i].nextElementSibling;
+    if(navigationSubmenu) navigationSubmenu.classList.toggle('navigation__submenu_show');
+}
+}
 // order
-
-const buttonsResumeOrders = document.querySelectorAll('.vacancy__button');
-console.log(buttonsResumeOrders)
-const resumeFormCross = document.querySelector('.form-resume__cross');
+const vacancyList = document.getElementById('vacancy-id');
+const buttonsResumeOrders = vacancyList.querySelectorAll('.vacancy__button');
+const resumeForm = document.getElementById('form-resume-inner-id');
+const resumeFormCross = resumeForm.querySelector('.form-resume__cross');
 for(let i = 0; i < buttonsResumeOrders.length; i++){
 buttonsResumeOrders[i].onclick = () => {
-    const resumeForm = document.querySelector('.form-resume-inner');
     resumeForm.classList.add('form-resume__opened');
 }
 }
 resumeFormCross.onclick = () => {
-    const resumeForm = document.querySelector('.form-resume-inner');
     resumeForm.classList.remove('form-resume__opened');
 }
