865e04a658709aeb3de402dbd1338b0fc709ce1d
sergJess <serg.fantasy@yandex.ru>
2025-08-13 19:54:48 +0300

    feat: add script for smooth scroll

--- src/js/index.js
+++ src/js/index.js
@@ -30,3 +30,35 @@ navigationLists[i].onclick = () => {
     if(navigationSubmenu) navigationSubmenu.classList.toggle('navigation__submenu_show');
 }
 }
+//scroll to anchors
+    function isNodeOrParent(target) {
+        let anchor = target;
+        if (target.parentNode.classList.contains('scrollTo')) {
+            anchor = target.parentNode;
+            return anchor
+        }
+        return anchor;
+    }
+    document.addEventListener('click', function(e) {
+        if (e.target.classList.contains('scrollTo') || e.target.parentNode.classList.contains('scrollTo')) {
+            e.preventDefault();
+            const scrollSpeed = 0.7;
+            let windowOffSetY = window.pageYOffset;
+            let anchorElement = isNodeOrParent(e.target).getAttribute('href').replace('#', '');
+            let toScrollElelement = document.getElementById(anchorElement);
+            let topCoordsOftoScrollElelement = toScrollElelement.getBoundingClientRect().top;
+            let start = null;
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) start = time;
+                let progress = time - start;
+                let windowCoordsToScrollY = (topCoordsOftoScrollElelement < 0) ? Math.max(windowOffSetY - progress / scrollSpeed, windowOffSetY + topCoordsOftoScrollElelement) : Math.min(windowOffSetY + progress / scrollSpeed, windowOffSetY + topCoordsOftoScrollElelement);
+                window.scrollTo(0, windowCoordsToScrollY);
+                if (windowCoordsToScrollY != windowOffSetY + topCoordsOftoScrollElelement) {
+                    requestAnimationFrame(step);
+                }
+
+            }
+        }
+    });
