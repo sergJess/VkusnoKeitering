fca7a9f6a5f0f7fbda24aa00e9242ed69cb61c11
sergJess <serg.fantasy@yandex.ru>
2025-08-13 19:34:34 +0300

    feat: add new styles and scripts

--- src/js/child-keiterings/child-keiterings.js
+++ src/js/child-keiterings/child-keiterings.js
@@ -31,3 +31,20 @@ navigationLists[i].onclick = () => {
     if(navigationSubmenu) navigationSubmenu.classList.toggle('navigation__submenu_show');
 }
 }
+//height in ready made solutions block linear-gradient
+const readyMeadeSolutionsBlock = document.getElementById('ready-made-solutions-id');
+function setReadyMadeSolutionsLinearGradient(
+    contentInnerBlock, color
+){
+const contentBlockHeight = contentInnerBlock.clientHeight;
+const imgInnerHeigh = contentInnerBlock.querySelector('.ready-made-solutions__img-inner').clientHeight;
+const textInnerHeight = contentInnerBlock.querySelector('.ready-made-solutions__text-title-block').clientHeight;
+if(window.innerWidth > 860){
+contentInnerBlock.style.backgroundImage = `linear-gradient(180deg, ${color} ${textInnerHeight - 8}px, #fff ${contentBlockHeight - textInnerHeight}px)`;
+}
+else{
+    contentInnerBlock.style.backgroundImage = `linear-gradient(180deg, ${color} ${textInnerHeight + imgInnerHeigh - 5}px, #fff ${contentBlockHeight - textInnerHeight - imgInnerHeigh}px)`;
+}
+}
+const readyMadeSolutionsContent = readyMeadeSolutionsBlock.querySelector('.ready-made-solutions__content');
+setReadyMadeSolutionsLinearGradient(readyMadeSolutionsContent,'#C76A6A');
