5cbc6545bbd3b87ee5d2e1b11ae1f473f764ef05
sergJess <serg.fantasy@yandex.ru>
2025-08-15 11:10:14 +0300

    feat: add scroll smooth for corporate clients

--- src/js/corporate-clients/corporate-clients.js
+++ src/js/corporate-clients/corporate-clients.js
@@ -1,51 +1,109 @@
-const header = document.getElementById('header-id');
+const header = document.getElementById("header-id");
 //menu
-const burger = header.querySelector('.navigation-burger');
-burger.onclick = ()=>{
-    const mobileMenu = header.querySelector('.header__block-nav');
-    mobileMenu.classList.add('header__block-nav_open');
-}
-const cross = header.querySelector('.navigation-cross__click');
+const burger = header.querySelector(".navigation-burger");
+burger.onclick = () => {
+  const mobileMenu = header.querySelector(".header__block-nav");
+  mobileMenu.classList.add("header__block-nav_open");
+};
+const cross = header.querySelector(".navigation-cross__click");
 cross.onclick = () => {
-    const mobileMenu = header.querySelector('.header__block-nav');
-    mobileMenu.classList.remove('header__block-nav_open');
-}
+  const mobileMenu = header.querySelector(".header__block-nav");
+  mobileMenu.classList.remove("header__block-nav_open");
+};
 //order-call-form
-const orderCallButtons = header.querySelectorAll('.order-call-button_btn');
-const crossOrderCallForm = document.getElementById('order-call-form-cross-id');
-const orderCallForm = document.getElementById('order-call-form-block-id');
+const orderCallButtons = header.querySelectorAll(".order-call-button_btn");
+const crossOrderCallForm = document.getElementById("order-call-form-cross-id");
+const orderCallForm = document.getElementById("order-call-form-block-id");
 crossOrderCallForm.onclick = () => {
-     orderCallForm.classList.remove('order-call-form-block_show');
-}
-for(let i = 0, length = orderCallButtons.length; i < length; i++){
-    orderCallButtons[i].onclick = () => {
-        orderCallForm.classList.add('order-call-form-block_show');
-    }
+  orderCallForm.classList.remove("order-call-form-block_show");
+};
+for (let i = 0, length = orderCallButtons.length; i < length; i++) {
+  orderCallButtons[i].onclick = () => {
+    orderCallForm.classList.add("order-call-form-block_show");
+  };
 }
 // navigation show submenu
-const navigationLists = header.querySelectorAll('.navigation__list');
-for(let i = 0, length = navigationLists.length; i < length; i++){
-navigationLists[i].onclick = () => {
+const navigationLists = header.querySelectorAll(".navigation__list");
+for (let i = 0, length = navigationLists.length; i < length; i++) {
+  navigationLists[i].onclick = () => {
     const navigationSubmenu = navigationLists[i].nextElementSibling;
-    if(navigationSubmenu) navigationSubmenu.classList.toggle('navigation__submenu_show');
-}
+    if (navigationSubmenu)
+      navigationSubmenu.classList.toggle("navigation__submenu_show");
+  };
 }
 // bid-form
-function bidFormRadioState(radioBlock){
-const radioProposal = radioBlock.querySelectorAll('.bid-form__radio-text');
-for(let i = 0; i < radioProposal.length; i++){
-    radioProposal[i].onclick = () =>{
-        for(let j = 0; j < radioProposal.length; j++){
-            const circleRadio = radioProposal[j].querySelector('.bid-form__input_radio-span'); 
-            circleRadio.classList.remove('bid-form__input_radio-span-active');
-        }
-        const currentCircleRadio = radioProposal[i].querySelector('.bid-form__input_radio-span');
-        currentCircleRadio.classList.add('bid-form__input_radio-span-active');
-    }
+function bidFormRadioState(radioBlock) {
+  const radioProposal = radioBlock.querySelectorAll(".bid-form__radio-text");
+  for (let i = 0; i < radioProposal.length; i++) {
+    radioProposal[i].onclick = () => {
+      for (let j = 0; j < radioProposal.length; j++) {
+        const circleRadio = radioProposal[j].querySelector(
+          ".bid-form__input_radio-span"
+        );
+        circleRadio.classList.remove("bid-form__input_radio-span-active");
+      }
+      const currentCircleRadio = radioProposal[i].querySelector(
+        ".bid-form__input_radio-span"
+      );
+      currentCircleRadio.classList.add("bid-form__input_radio-span-active");
+    };
+  }
 }
-}
-const bidForm = document.getElementById('bid-form-id');
-const radioProposalInner = bidForm.querySelector('.bid-form__radio-inner-proposal');
-const radioDeliveryInner = bidForm.querySelector('.bid-form__radio-inner-delivery');
+const bidForm = document.getElementById("bid-form-id");
+const radioProposalInner = bidForm.querySelector(
+  ".bid-form__radio-inner-proposal"
+);
+const radioDeliveryInner = bidForm.querySelector(
+  ".bid-form__radio-inner-delivery"
+);
 bidFormRadioState(radioProposalInner);
 bidFormRadioState(radioDeliveryInner);
+//scroll to anchors
+function isNodeOrParent(target) {
+  let anchor = target;
+  if (target.parentNode.classList.contains("scroll-to")) {
+    anchor = target.parentNode;
+    return anchor;
+  }
+  return anchor;
+}
+document.addEventListener("click", function (e) {
+  if (
+    e.target.classList.contains("scroll-to") ||
+    e.target.parentNode.classList.contains("scroll-to")
+  ) {
+    e.preventDefault();
+    const scrollSpeed = 0.7;
+    let windowOffSetY = window.pageYOffset;
+    let anchorElement = isNodeOrParent(e.target)
+      .getAttribute("href")
+      .replace("#", "");
+    let toScrollElelement = document.getElementById(anchorElement);
+    let topCoordsOftoScrollElelement =
+      toScrollElelement.getBoundingClientRect().top;
+    let start = null;
+    requestAnimationFrame(step);
+
+    function step(time) {
+      if (start === null) start = time;
+      let progress = time - start;
+      let windowCoordsToScrollY =
+        topCoordsOftoScrollElelement < 0
+          ? Math.max(
+              windowOffSetY - progress / scrollSpeed,
+              windowOffSetY + topCoordsOftoScrollElelement
+            )
+          : Math.min(
+              windowOffSetY + progress / scrollSpeed,
+              windowOffSetY + topCoordsOftoScrollElelement
+            );
+      window.scrollTo(0, windowCoordsToScrollY);
+      if (
+        windowCoordsToScrollY !=
+        windowOffSetY + topCoordsOftoScrollElelement
+      ) {
+        requestAnimationFrame(step);
+      }
+    }
+  }
+});
