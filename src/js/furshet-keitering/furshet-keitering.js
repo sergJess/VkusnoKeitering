5f82116d18a0fd15e6e39fda02b3521e858a4b70
sergJess <serg.fantasy@yandex.ru>
2025-08-17 11:30:46 +0300

    feat: add new scripts and classes

--- src/js/furshet-keitering/furshet-keitering.js
+++ src/js/furshet-keitering/furshet-keitering.js
@@ -1,49 +1,108 @@
-const header = document.getElementById('header-id');
+const header = document.getElementById("header-id");
 //menu
-const burger = header.querySelector('.navigation-burger');
-burger.onclick = ()=>{
-    const mobileMenu = header.querySelector('.header__block-nav');
-    mobileMenu.classList.add('header__block-nav_open');
-}
-const cross = header.querySelector('.navigation-cross__click');
+const burger = header.querySelector(".navigation-burger");
+burger.onclick = () => {
+  const mobileMenu = header.querySelector(".header__block-nav");
+  mobileMenu.classList.add("header__block-nav_open");
+};
+const cross = header.querySelector(".navigation-cross__click");
 cross.onclick = () => {
-    const mobileMenu = header.querySelector('.header__block-nav');
-    mobileMenu.classList.remove('header__block-nav_open');
-}
+  const mobileMenu = header.querySelector(".header__block-nav");
+  mobileMenu.classList.remove("header__block-nav_open");
+};
 //order-call-form
-const orderCallButtons = header.querySelectorAll('.order-call-button_btn');
-const crossOrderCallForm = document.getElementById('order-call-form-cross-id');
-const orderCallForm = document.getElementById('order-call-form-block-id');
+const orderCallButtons = header.querySelectorAll(".order-call-button_btn");
+const crossOrderCallForm = document.getElementById("order-call-form-cross-id");
+const orderCallForm = document.getElementById("order-call-form-block-id");
 crossOrderCallForm.onclick = () => {
-     orderCallForm.classList.remove('order-call-form-block_show');
-}
-for(let i = 0, length = orderCallButtons.length; i < length; i++){
-    orderCallButtons[i].onclick = () => {
-        orderCallForm.classList.add('order-call-form-block_show');
-    }
+  orderCallForm.classList.remove("order-call-form-block_show");
+};
+for (let i = 0, length = orderCallButtons.length; i < length; i++) {
+  orderCallButtons[i].onclick = () => {
+    orderCallForm.classList.add("order-call-form-block_show");
+  };
 }
 // navigation show submenu
-const navigationLists = header.querySelectorAll('.navigation__list');
-for(let i = 0, length = navigationLists.length; i < length; i++){
-navigationLists[i].onclick = () => {
+const navigationLists = header.querySelectorAll(".navigation__list");
+for (let i = 0, length = navigationLists.length; i < length; i++) {
+  navigationLists[i].onclick = () => {
     const navigationSubmenu = navigationLists[i].nextElementSibling;
-    if(navigationSubmenu) navigationSubmenu.classList.toggle('navigation__submenu_show');
-}
+    if (navigationSubmenu)
+      navigationSubmenu.classList.toggle("navigation__submenu_show");
+  };
 }
 //height in ready made solutions block linear-gradient
-const readyMeadeSolutionsBlock = document.getElementById('ready-made-solutions-id');
-function setReadyMadeSolutionsLinearGradient(
-    contentInnerBlock, color
-){
-const contentBlockHeight = contentInnerBlock.clientHeight;
-const imgInnerHeigh = contentInnerBlock.querySelector('.ready-made-solutions__img-inner').clientHeight;
-const textInnerHeight = contentInnerBlock.querySelector('.ready-made-solutions__text-title-block').clientHeight;
-if(window.innerWidth > 860){
-contentInnerBlock.style.backgroundImage = `linear-gradient(180deg, ${color} ${textInnerHeight - 8}px, #fff ${contentBlockHeight - textInnerHeight}px)`;
+const readyMeadeSolutionsBlock = document.getElementById(
+  "ready-made-solutions-id"
+);
+function setReadyMadeSolutionsLinearGradient(contentInnerBlock, color) {
+  const contentBlockHeight = contentInnerBlock.clientHeight;
+  const imgInnerHeigh = contentInnerBlock.querySelector(
+    ".ready-made-solutions__img-inner"
+  ).clientHeight;
+  const textInnerHeight = contentInnerBlock.querySelector(
+    ".ready-made-solutions__text-title-block"
+  ).clientHeight;
+  if (window.innerWidth > 860) {
+    contentInnerBlock.style.backgroundImage = `linear-gradient(180deg, ${color} ${
+      textInnerHeight - 8
+    }px, #fff ${contentBlockHeight - textInnerHeight}px)`;
+  } else {
+    contentInnerBlock.style.backgroundImage = `linear-gradient(180deg, ${color} ${
+      textInnerHeight + imgInnerHeigh - 5
+    }px, #fff ${contentBlockHeight - textInnerHeight - imgInnerHeigh}px)`;
+  }
 }
-else{
-    contentInnerBlock.style.backgroundImage = `linear-gradient(180deg, ${color} ${textInnerHeight + imgInnerHeigh - 5}px, #fff ${contentBlockHeight - textInnerHeight - imgInnerHeigh}px)`;
+const readyMadeSolutionsContent = readyMeadeSolutionsBlock.querySelector(
+  ".ready-made-solutions__content"
+);
+setReadyMadeSolutionsLinearGradient(readyMadeSolutionsContent, "#698155");
+//scroll to anchors
+function isNodeOrParent(target) {
+  let anchor = target;
+  if (target.parentNode.classList.contains("scroll-to")) {
+    anchor = target.parentNode;
+    return anchor;
+  }
+  return anchor;
 }
-}
-const readyMadeSolutionsContent = readyMeadeSolutionsBlock.querySelector('.ready-made-solutions__content');
-setReadyMadeSolutionsLinearGradient(readyMadeSolutionsContent,'#698155');
+document.addEventListener("click", function (e) {
+  if (
+    e.target.classList.contains("scroll-to") ||
+    e.target.parentNode.classList.contains("scroll-to")
+  ) {
+    e.preventDefault();
+    const scrollSpeed = 0.7;
+    let windowOffSetY = window.pageYOffset;
+    let anchorElement = isNodeOrParent(e.target)
+      .getAttribute("href")
+      .replace("#", "");
+    let toScrollElelement = document.getElementById(anchorElement);
+    let topCoordsOftoScrollElelement =
+      toScrollElelement.getBoundingClientRect().top;
+    let start = null;
+    requestAnimationFrame(step);
+
+    function step(time) {
+      if (start === null) start = time;
+      let progress = time - start;
+      let windowCoordsToScrollY =
+        topCoordsOftoScrollElelement < 0
+          ? Math.max(
+              windowOffSetY - progress / scrollSpeed,
+              windowOffSetY + topCoordsOftoScrollElelement
+            )
+          : Math.min(
+              windowOffSetY + progress / scrollSpeed,
+              windowOffSetY + topCoordsOftoScrollElelement
+            );
+      window.scrollTo(0, windowCoordsToScrollY);
+      if (
+        windowCoordsToScrollY !=
+        windowOffSetY + topCoordsOftoScrollElelement
+      ) {
+        requestAnimationFrame(step);
+      }
+    }
+  }
+});
